9594ca3257b44f373cce495d239b7a4d3370519f
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-02-19 22:46:22 +0300

    added simple animations

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+let animation01 = anime({
+  targets: '#rectangleone',
+  autoplay: true,
+  duration: 4000,
+  direction: 'alternate',
+  translateX: -1920,
+  loop: true,
+  easing: 'easeInOutSine'
+})
+
+let animation02 = anime({
+  targets: '#rectangletwo',
+  autoplay: true,
+  duration: 6000,
+  direction: 'alternate',
+  translateX: 1920,
+  loop: true,
+  easing: 'easeInOutSine'
+})
+
+let animation03 = anime({
+  targets: '#rectanglethree',
+  autoplay: true,
+  duration: 5000,
+  direction: 'alternate',
+  translateX: -1920,
+  loop: true,
+  easing: 'easeInOutSine'
+})
