649c3b6de8af25ee82f1174f422364738fd5d536
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-02-19 23:26:31 +0300

    more animations

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,7 @@ let animation01 = anime({
   direction: 'alternate',
   translateX: -1920,
   loop: true,
-  easing: 'easeInOutSine'
+  easing: 'easeInOutSine',
 })
 
 let animation02 = anime({
@@ -15,15 +15,59 @@ let animation02 = anime({
   direction: 'alternate',
   translateX: 1920,
   loop: true,
-  easing: 'easeInOutSine'
+  easing: 'easeInOutSine',
 })
 
 let animation03 = anime({
-  targets: '#rectanglethree',
+  targets: '#squareyellow',
   autoplay: true,
-  duration: 5000,
-  direction: 'alternate',
-  translateX: -1920,
   loop: true,
-  easing: 'easeInOutSine'
+  direction: 'alternate',
+  translateY: [
+    { value: 594, duration: 1000, delay: 500 },
+  // { value: -190, duration: 1000, delay: 500 },
+  // { value: 0, duration: 1000, delay: 500 },
+  ],
+  // translateX: [
+  // { value: 786, duration: 1000, delay: 500 },
+  // { value: -786, duration: 1000, delay: 500 },
+  // { value: 0, duration: 1000, delay: 500 },
+  // ],
+  // keyframes: [
+  //     {translateY: 594},
+  //     {translateX: 780},
+  //     {translateY: 40},
+  //     {translateX: 0},
+  //     {translateY: 0}
+  //   ],
+  //   duration: 15000,
+  //   easing: 'easeOutElastic(1, .8)',
+  //   loop: true,
+})
+
+// let animation04 = anime({
+//   targets: '#squareyellow',
+//   autoplay: true,
+//   translateX: [
+//     { value: 786, duration: 1000, delay: 500 },
+//   ],
+// })
+
+let animation05 = anime({
+  targets: '#squaregreen',
+  autoplay: true,
+  translateY: [
+    { value: -495, duration: 3000, delay: 1000 },
+  ],
+  loop: true,
+  direction: 'alternate',
+})
+
+let animation06 = anime({
+  targets: '#circle',
+  loop: true,
+  autoplay: true,
+  translateX: [
+    { value: -525, duration: 1000, delay: 1000 },
+  ],
 })
